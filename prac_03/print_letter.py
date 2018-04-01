d1ad2f60abf9e6e9391200c345f6cef62a156a0c
CDivyaPrabha <31094031+CDivyaPrabha@users.noreply.github.com>
2018-04-01 21:13:43 +0800

    Convert fahrenheit to celsius and vice versa.

--- prac_03/print_letter.py
+++ prac_03/print_letter.py
@@ -1,11 +1,26 @@
-"""Divya Prabha Chandrasekaran"""
-flag = 0
-while flag == 0:
-    name = input("Enter your name: ")
-    for char in name:
-        if char.isupper() or char.islower():
-            flag = 1
-            break
-    if flag == 0:
-        print("Incorrect input")
-print(name[0:len(name):2])
+def main():
+    """Divya Prabha Chandrasekaran"""
+    name = get_name()
+    number = int(input('Enter a number: '))
+    print_letters(name, number)
+
+
+def print_letters(name, number):
+    for i in range(number-1, len(name), number):
+        print(name[i])
+
+
+def get_name():
+    flag = 0
+    while flag == 0:
+        name = input("Enter your name: ")
+        for char in name:
+            if char.isupper() or char.islower():
+                flag = 1
+                break
+        if flag == 0:
+            print("Incorrect input")
+    return name
+
+
+main()
