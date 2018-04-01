46e03edfc5318ddfbd5103d3e2b8dec569fb4a13
CDivyaPrabha <31094031+CDivyaPrabha@users.noreply.github.com>
2018-04-01 21:14:19 +0800

    Convert fahrenheit to celsius and vice versa in different functions.

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -8,14 +8,26 @@ F - Convert Fahrenheit to Celsius
 Q - Quit"""
 print(MENU)
 choice = input(">>> ").upper()
+
+
+def celsius_to_fahrenheit():
+    global fahrenheit
+    fahrenheit = celsius * 9.0 / 5 + 32
+
+
+def fahrenheit_to_celsius():
+    global celsius
+    celsius = (fahrenheit - 32) / 9 * 5
+
+
 while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
+        celsius_to_fahrenheit()
         print("Result: {:.2f} F".format(fahrenheit))
     elif choice == "F":
         fahrenheit = float(input("Fahrenheit: "))
-        celsius = (fahrenheit - 32)/9*5
+        fahrenheit_to_celsius()
         print("Result: {:.2f} C".format(celsius))
     else:
         print("Invalid option")
