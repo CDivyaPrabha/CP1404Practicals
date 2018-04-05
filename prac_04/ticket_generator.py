a5fab53b391aa9c6e76e44ac559cd1882cbdacef
CDivyaPrabha <31094031+CDivyaPrabha@users.noreply.github.com>
2018-04-05 15:27:38 +0800

    Generate quick picks

--- prac_04/ticket_generator.py
+++ prac_04/ticket_generator.py
@@ -1,20 +1,24 @@
 import random
-numbers = []
-flag = 0
-picks = input("How many quick picks do you wish to generate? ")
-numbers[0] = random.randint(1, 46)
-for i in range(1, int(picks) + 1):
-    for x in range(5):
-        while flag == 0:
-            this_no = random.randint(1, 46)
-            if this_no in numbers:
-                flag = 0
-            else:
-                flag = 1
-        numbers = numbers.append(this_no)
-        flag = 0
-    numbers.sort()
-    print(numbers)
+
+
+def main():
+    picks = input("How many quick picks do you wish to generate? ")
     numbers = []
+    for i in range(1, int(picks) + 1):
+        numbers = get_unique_list()
+        numbers.sort()
+        print(numbers)
 
 
+def get_unique_list():
+    numbers = []
+    numbers.append(random.randint(1, 46))
+    for x in range(5):
+        this_no = random.randint(1, 46)
+        while this_no in numbers:
+            this_no = random.randint(1, 46)
+        numbers.append(this_no)
+    return numbers
+
+
+main()
